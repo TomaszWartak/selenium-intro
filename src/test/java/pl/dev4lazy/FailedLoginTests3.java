8a5af142f050cf3f35c7811aa7ffc7c8f33e0299
tomaszwartak <tomasz.wartak@gmail.com>
2024-01-27 12:42:47 +0100

    + cd

--- src/test/java/pl/dev4lazy/FailedLoginTests3.java
+++ src/test/java/pl/dev4lazy/FailedLoginTests3.java
@@ -1,14 +1,9 @@
 package pl.dev4lazy;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,15 +11,13 @@ import pl.dev4lazy.locators.Locator;
 
 import java.time.Duration;
 
-import static org.testng.Assert.assertEquals;
-
 /*
 Wejdź na stronę http://przyklady.javastart.pl/jpetstore/
 Kliknij w link Sign In
 Na stronie logowania podając nieprawidłowy login i hasło spróbuj się zalogować
 Test powinien skończyć się sprawdzeniem czy komunikat „Invalid username or password. Signon failed.” został wyświetlony.
  */
-public class FailedLoginTests {
+public class FailedLoginTests3 {
     final String WIN_DRIVER_PATH = "C:\\drivers\\chromedriver\\chromedriver.exe";
     final String MAC_DRIVER_PATH = "/usr/local/bin/chromedriver";
     final String APP_URL = "http://przyklady.javastart.pl/jpetstore/";
@@ -35,30 +28,33 @@ public class FailedLoginTests {
 //        String driverPath = WIN_DRIVER_PATH;
         System.setProperty("webdriver.chrome.driver", driverPath );
         driver = new ChromeDriver();
+//      Ustawienie Implicit Wait na 10 sekund
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 //        Wejdź na stronę http://przyklady.javastart.pl/jpetstore/
         driver.navigate().to( APP_URL );
     }
 
     @Test
     public void test() {
+//        Kliknij w link Enter the Store
+        enterTheStoreClick();
 //        Kliknij w link Sign In
-        WebElement enterTheStoreLink = driver.findElement(
-                By.xpath( new Locator().a().withInnerText("Enter the Store").get() )
-        );
-        enterTheStoreLink.click();
-        Wait<WebDriver> signInLinkWait = new WebDriverWait(driver, Duration.ofSeconds(5) );
-        WebElement signInLink = signInLinkWait.until(
-                ExpectedConditions.presenceOfElementLocated(
-                        By.xpath( new Locator().a().withInnerText("Sign In").get() )
-                )
-        );
-        signInLink.click();
+        signInClick();
 //        Na stronie logowania podając nieprawidłowy login i hasło spróbuj się zalogować
-        Wait<WebDriver> userNameInputWait = new WebDriverWait(driver, Duration.ofSeconds(5) );
-        WebElement userNameInput = userNameInputWait.until(
-                ExpectedConditions.presenceOfElementLocated(
-                        By.xpath( new Locator().input().withName("username").get() )
-                )
+        processIncorrectLoginAndPassword();
+//        Test powinien skończyć się sprawdzeniem czy komunikat „Invalid username or password. Signon failed.” został wyświetlony.
+        checkSignonFailedMessage();
+    }
+
+    private void checkSignonFailedMessage() {
+        WebElement messageLi = driver.findElement(
+                By.xpath( new Locator().li().withInnerText("Invalid username or password.  Signon failed.").get() )
+        );
+    }
+
+    private void processIncorrectLoginAndPassword() {
+        WebElement userNameInput = driver.findElement(
+                By.xpath( new Locator().input().withName("username").get() )
         );
         WebElement passwordInput = driver.findElement(
                 By.xpath( new Locator().input().withName("password").get() )
@@ -69,14 +65,20 @@ public class FailedLoginTests {
         userNameInput.sendKeys( "chuj");
         passwordInput.sendKeys( "chujp");
         signOnInput.click();
-//        Test powinien skończyć się sprawdzeniem czy komunikat „Invalid username or password. Signon failed.” został wyświetlony.
-        Wait<WebDriver> messageLiWait = new WebDriverWait(driver, Duration.ofSeconds(5) );
-        WebElement messageLi = messageLiWait.until(
-                ExpectedConditions.presenceOfElementLocated(
-                        By.xpath( new Locator().li().withInnerText("Invalid username or password.  Signon failed.").get() )
-                )
-        );
+    }
 
+    private void signInClick() {
+        WebElement signInLink = driver.findElement(
+                By.xpath( new Locator().a().withInnerText("Sign In").get() )
+        );
+        signInLink.click();
+    }
+
+    private void enterTheStoreClick() {
+        WebElement enterTheStoreLink = driver.findElement(
+                By.xpath( new Locator().a().withInnerText("Enter the Store").get() )
+        );
+        enterTheStoreLink.click();
     }
 
 
